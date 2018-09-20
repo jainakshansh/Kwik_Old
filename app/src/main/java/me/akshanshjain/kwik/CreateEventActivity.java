1a584bf9cf117e8dfd9bb3476839c450b0651d52
Akshansh Jain <akshanshjn9@gmail.com>
2018-09-21 00:15:45 +0530

    Enhanced layout. Started logic building.

---
+++ app/src/main/java/me/akshanshjain/kwik/CreateEventActivity.java
@@ -0,0 +1,13 @@
+package me.akshanshjain.kwik;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event);
+    }
+}
--- app/src/main/java/me/akshanshjain/kwik/MainActivity.java
+++ app/src/main/java/me/akshanshjain/kwik/MainActivity.java
@@ -1,13 +1,44 @@
 package me.akshanshjain.kwik;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView eventsRecycler;
+    private FloatingActionButton createEvent;
+    private TextView upcomingLabel;
+
+    private Typeface QLight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Setting up the toolbar for the activity.
+        Toolbar toolbar = findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+
+        //Setting the app name on the Toolbar.
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(getString(R.string.app_name));
+        }
+
+        /*
+        Initialising and referencing views from the XML.
+        */
+        eventsRecycler = findViewById(R.id.recycler_view_main);
+        createEvent = findViewById(R.id.fab_main);
+        upcomingLabel = findViewById(R.id.upcoming_label_main);
+
+        //Setting the typeface on the labels.
+        QLight = Typeface.createFromAsset(getAssets(), "fonts/Quicksand-Light.ttf");
+        upcomingLabel.setTypeface(QLight);
     }
 }
