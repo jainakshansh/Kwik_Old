cd265fef2ea821d101f2150ce929dd8dcd011774
Akshansh Jain <akshanshjn9@gmail.com>
2018-09-22 19:26:04 +0530

    Added code to retrieve events from Firebase.

--- app/src/main/java/me/akshanshjain/kwik/Activities/MainActivity.java
+++ app/src/main/java/me/akshanshjain/kwik/Activities/MainActivity.java
@@ -2,14 +2,34 @@ package me.akshanshjain.kwik.Activities;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.LinearSnapHelper;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SnapHelper;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.akshanshjain.kwik.Adapters.EventsAdapter;
+import me.akshanshjain.kwik.Objects.EventItem;
 import me.akshanshjain.kwik.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView eventsRecycler;
     private FloatingActionButton createEvent;
     private TextView upcomingLabel;
+
+    private List<EventItem> eventItemList;
+    private EventsAdapter eventsAdapter;
 
     private Typeface QLight;
 
@@ -50,6 +73,75 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), CreateEventActivity.class));
+            }
+        });
+
+        //Setting up the Recycler View.
+        eventItemList = new ArrayList<>();
+        eventsAdapter = new EventsAdapter(this, eventItemList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        eventsRecycler.setLayoutManager(layoutManager);
+        eventsRecycler.setItemAnimator(new DefaultItemAnimator());
+        eventsRecycler.setAdapter(eventsAdapter);
+
+        /*
+        This centers the child elements w.r.t. the screen on scroll.
+        Makes sure that an item is always fully visible and not partial.
+        */
+        SnapHelper snapHelper = new LinearSnapHelper();
+        snapHelper.attachToRecyclerView(eventsRecycler);
+
+        /*
+        Checking if the mobile is connected to the internet.
+        If connected, we retrieve the data from the Firebase RealTime Database.
+        */
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo != null) {
+            //Calling the function to get the data.
+            getEventsData();
+        }
+    }
+
+    /*
+    This function gets a reference to the firebase database.
+    Then is called everytime the data changes.
+    The code for checking network connectivity is included in the onCreate function.
+    */
+    private void getEventsData() {
+        //Getting an instance of the database.
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        final DatabaseReference databaseReference = firebaseDatabase.getReference();
+
+        /*
+        Loading the data from the database whenever any value changes.
+        */
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                EventItem eventItem = dataSnapshot.getValue(EventItem.class);
+                eventItemList.add(eventItem);
+                eventsAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                eventsAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+                eventsAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                eventsAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(MainActivity.this, "Error connecting. Please check your internet connection!", Toast.LENGTH_SHORT).show();
             }
         });
     }
