0b1224022ca5cc0769acf1b3b9d317903e5269fb
Akshansh Jain <akshanshjn9@gmail.com>
2018-10-02 22:48:27 +0530

    Designed layout for Details activity.

--- app/src/main/java/me/akshanshjain/kwik/EventDetailsActivity.java
+++ app/src/main/java/me/akshanshjain/kwik/EventDetailsActivity.java
@@ -1,7 +1,8 @@
 package me.akshanshjain.kwik;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class EventDetailsActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class EventDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_event_details);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle("Eating");
+        }
     }
 }
